0cfe2e2293215059a824ce4c73550b1cbb9e6c0d
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2021-03-20 16:06:10 +0300

    add location to tournament page, refactor render

--- src/pages/Tournaments/Tournament/render.jsx
+++ src/pages/Tournaments/Tournament/render.jsx
@@ -10,82 +10,58 @@ import PropTypes from 'prop-types'
 const {Title, Paragraph} = Typography;
 
 
-const renderLoading = (props) => {
-    return (
-        <BasePage>
-            <Row style={{marginBottom: 8}}>
-                <Space size='small' align='center'>
-                    <Skeleton active avatar={{size: 64}} paragraph={{rows: 0}} title={false}/>
-                    <Skeleton.Button active size={46} style={{width: 200}}/>
-                </Space>
-            </Row>
+function TournamentPageRender(props) {
+    const isLoading = props.tournamentData === undefined;
 
-            <Row>
-                <Skeleton title={false}/>
-            </Row>
-
-            <Tabs tabBarStyle={{marginBottom: 32}} defaultActiveKey={props.section} onChange={props.onSectionChange}>
-                <Tabs.TabPane tab={'Сетка'} key={TournamentPageRender.sections[0]}>
-                    <Spin/>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={'Таблица'} key={TournamentPageRender.sections[1]}>
-                    <Spin/>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={'История'} key={TournamentPageRender.sections[2]}>
-                    <Spin/>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab={'Настройки'} key={TournamentPageRender.sections[3]}>
-                    <Spin/>
-                </Tabs.TabPane>
-            </Tabs>
-        </BasePage>
-    )
-}
-
-const renderLoaded = (props) => {
     return (
         <BasePage>
             <Row style={{marginBottom: 8}}>
                 <Space size='large' align='center'>
-                    <Avatar size={64}/>
-                    <Title style={{marginBottom: 0}}>{props.tournamentData.name || ''}</Title>
+                    {isLoading ?
+                        <>
+                            <Skeleton active avatar={{size: 64}} paragraph={{rows: 0}} title={false}/>
+                            <Skeleton.Button active size={46} style={{width: 200}}/>
+                        </> :
+                        <>
+                            <Avatar size={64}/>
+                            <Title style={{marginBottom: 0}}>{props.tournamentData.name || ''}</Title>
+                        </>
+                    }
                 </Space>
             </Row>
 
-            <Row>
-                <Paragraph>
-                    {props?.tournamentData?.about || ''}
-                </Paragraph>
-            </Row>
+            {isLoading ?
+                <Skeleton title={false}/> :
+                <Typography>
+                    <Paragraph>
+                        Место проведения: {props?.tournamentData?.location}
+                    </Paragraph>
+                    <Paragraph>
+                        {props?.tournamentData?.about || ''}
+                    </Paragraph>
+                </Typography>
+            }
 
             <Tabs tabBarStyle={{marginBottom: 32}} defaultActiveKey={props.section} onChange={props.onSectionChange}>
                 <Tabs.TabPane tab={'Сетка'} key={TournamentPageRender.sections[0]}>
-                    <TournamentGridRender/>
+                    {isLoading ? <Spin/> : <TournamentGridRender/>}
                 </Tabs.TabPane>
                 <Tabs.TabPane tab={'Таблица'} key={TournamentPageRender.sections[1]}>
-                    <TournamentTableRender/>
+                    {isLoading ? <Spin/> : <TournamentTableRender/>}
                 </Tabs.TabPane>
                 <Tabs.TabPane tab={'История'} key={TournamentPageRender.sections[2]}>
-                    <TournamentHistoryRender/>
+                    {isLoading ? <Spin/> : <TournamentHistoryRender/>}
                 </Tabs.TabPane>
                 <Tabs.TabPane tab={'Настройки'} key={TournamentPageRender.sections[3]}>
-                    <TournamentSettingsRender
-                        tournamentId={props.tournamentData.id}
-                        teams={props.tournamentData.teams || []}/>
+                    {isLoading ? <Spin/> :
+                        <TournamentSettingsRender
+                            tournamentId={props.tournamentData.id}
+                            teams={props.tournamentData.teams || []}/>}
                 </Tabs.TabPane>
             </Tabs>
 
         </BasePage>
     )
-}
-
-function TournamentPageRender(props) {
-    const isLoading = props.tournamentData === undefined;
-
-    if (isLoading) {
-        return renderLoading(props);
-    }
-    return renderLoaded(props);
 }
 
 TournamentPageRender.propTypes = {
