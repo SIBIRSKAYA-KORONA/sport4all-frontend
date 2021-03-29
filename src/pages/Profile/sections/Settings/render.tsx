6ada2887c812f64d7b51ad41709b56ae83a4136e
Egor Bedov <bedovegor2203@gmail.com>
2021-03-29 16:21:31 +0300

    Simple profile edit form

--- src/pages/Profile/sections/Settings/render.tsx
+++ src/pages/Profile/sections/Settings/render.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { Typography, Spin, Form, Input } from 'antd';
+import { Typography, Form, Input, Button, message } from 'antd';
 const { Title } = Typography;
-const { Item } = Form;
 
-import UserModel from 'Models/UserModel';
 import { User } from 'Utils/types';
 
 
@@ -14,15 +12,49 @@ interface IProps {
 }
 
 const SettingsProfileSection = (props:IProps):JSX.Element => {
-    const [userInfo, setUserInfo] = useState({...props.user});
+    const [loading, setLoading] = useState(false);
 
+    const saveInfo = (values) => {
+        setLoading(true);
+        console.log(values);
+        setTimeout(() => {
+            message.info('Метод не подключен');
+            setLoading(false);
+        }, 1000);
+    };
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+    };
     return (<>
         <Title level={2}>Настройки</Title>
-        {props.user
-            ? <Form>
-                <Item><Input/></Item>
+        {props.user &&
+            <Form
+                {...layout}
+                onFinish={saveInfo}
+                initialValues={{...props.user}}
+            >
+                <Form.Item label='Имя' name='name' rules={[{ required: true, message: 'Иван' }]}>
+                    <Input/>
+                </Form.Item>
+                <Form.Item label='Фамилия' name='surname' rules={[{ required: true, message: 'Иванов' }]}>
+                    <Input/>
+                </Form.Item>
+                <Form.Item label='Никнейм' name='nickname' rules={[{ required: true, message: 'Введите ваш никнейм' }]}>
+                    <Input/>
+                </Form.Item>
+                <Form.Item label='Почта' name='email' rules={[{ required: true, message: 'ivan.ivanov@ivan.ivanov' }]}>
+                    <Input/>
+                </Form.Item>
+                <Form.Item label='О себе' name='about'
+                    rules={[{ message: 'Заслуженный тренер РФ и Олимпийский чемпион по литрболу' }]}>
+                    <Input.TextArea/>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" loading={loading}>Сохранить</Button>
+                </Form.Item>
             </Form>
-            : <Spin/>
         }
     </>);
 };
