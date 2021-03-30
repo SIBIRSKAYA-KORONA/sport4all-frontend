128988306555dd94343149d9190bb0d20fb08f90
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2021-03-30 22:21:51 +0300

    add basic avatar upload

--- src/pages/Tournaments/Tournament/sections/Settings/PublicInfo/render.jsx
+++ src/pages/Tournaments/Tournament/sections/Settings/PublicInfo/render.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Button, Form, Select} from 'antd';
+import {Avatar, Button, Form, Row, Select, Upload} from 'antd';
 import PublicInfoFormItemsRender from 'Pages/Tournaments/Shared/PublicInfoFormItems/render';
 import PropTypes from 'prop-types'
 
@@ -10,38 +10,65 @@ const layout = {
 
 function PublicInfoRender(props) {
     return (
-        /*TODO: call api*/
-        <Form
-            {...layout}
-            onFinish={props.onSubmit}
-            initialValues={{
-                name: props.tournamentData.name,
-                about: props.tournamentData.about,
-                systemType: props.tournamentData.system,
-                location: props.tournamentData.location,
-                status: props.tournamentData.status,
-            }}
-        >
-            <PublicInfoFormItemsRender/>
-
-            <Form.Item
-                label="Статус"
-                name="status"
+        <Row>
+            <Form
+                {...layout}
+                onFinish={props.onSubmit}
+                initialValues={{
+                    name: props.tournamentData.name,
+                    about: props.tournamentData.about,
+                    systemType: props.tournamentData.system,
+                    location: props.tournamentData.location,
+                    status: props.tournamentData.status,
+                }}
             >
-                <Select>
-                    <Select.Option value={1}>{PublicInfoRender.statuses[1]}</Select.Option>
-                    <Select.Option value={2}>{PublicInfoRender.statuses[2]}</Select.Option>
-                    <Select.Option value={3}>{PublicInfoRender.statuses[3]}</Select.Option>
-                    <Select.Option value={4}>{PublicInfoRender.statuses[4]}</Select.Option>
-                </Select>
-            </Form.Item>
+                <PublicInfoFormItemsRender/>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Сохранить
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item
+                    label="Статус"
+                    name="status"
+                >
+                    <Select>
+                        <Select.Option value={1}>{PublicInfoRender.statuses[1]}</Select.Option>
+                        <Select.Option value={2}>{PublicInfoRender.statuses[2]}</Select.Option>
+                        <Select.Option value={3}>{PublicInfoRender.statuses[3]}</Select.Option>
+                        <Select.Option value={4}>{PublicInfoRender.statuses[4]}</Select.Option>
+                    </Select>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Сохранить
+                    </Button>
+                </Form.Item>
+            </Form>
+            <Upload
+                listType="picture-card"
+                showUploadList={false}
+                customRequest={async (fileData)=>{
+                    const formData = new FormData();
+                    formData.append("tournamentId", props.tournamentData.id);
+                    formData.append("attach", fileData.file);
+                    const response = await fetch('https://sport4all.tech/api/attachments', {
+                        method: 'POST',
+                        body: formData,
+                        mode: 'cors',
+                        credentials: 'include'
+                    });
+
+                }}
+            >
+                <Avatar style={{ width: '100%', height: '100%' }}/>
+            </Upload>
+            {/*<Upload*/}
+            {/*    name="avatar"*/}
+            {/*    listType="picture-card"*/}
+            {/*    className="avatar-uploader"*/}
+            {/*    showUploadList={false}*/}
+            {/*>*/}
+            {/*    <img  alt="avatar" style={{ width: '100%' }} />*/}
+            {/*</Upload>*/}
+        </Row>
     )
 }
 
