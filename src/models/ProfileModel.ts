d1a7de31b9b0d427fe2d3162c92f59e062c1340e
Egor Bedov <bedovegor2203@gmail.com>
2021-04-03 13:14:23 +0300

    fix fetch in profilemodel

--- src/models/ProfileModel.ts
+++ src/models/ProfileModel.ts
@@ -4,7 +4,7 @@ import HttpStatusCode from 'Utils/httpErrors';
 
 class ProfileModel {
     static async loadSkills(pid: number): Promise<HttpStatusCode | Array<Skill>> {
-        return Network.fetch(Network.paths.profile.skills(pid))
+        return Network.fetchGet(Network.paths.profile.skills(pid))
             .then(res => {
                 if (res.status >= 400) return res.status as HttpStatusCode;
                 return res.json() as Array<Skill>;
