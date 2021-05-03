4733df89b3da89bab76753398044ab5172c438a6
Egor Bedov <bedovegor2203@gmail.com>
2021-05-03 19:10:23 +0300

    rename

--- src/pages/Landing/render.tsx
+++ src/pages/Landing/render.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 
 import Thanks from 'Components/Thanks/render';
 import BasePage from 'Components/BasePage/render';
+import { RouteComponentProps } from 'react-router-dom';
 
-const HomePage = (props) => (
+
+const LandingPage = (props:RouteComponentProps):JSX.Element => (
     <BasePage {...props}>
         <Thanks/>
     </BasePage>
-)
+);
 
-export default HomePage;
+export default LandingPage;
