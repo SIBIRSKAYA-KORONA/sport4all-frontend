b58577c5c7d9fdbab16e79c4c90621e22aa5425d
Egor Bedov <bedovegor2203@gmail.com>
2021-05-03 19:12:05 +0300

    Remove thanks component

--- src/pages/Landing/render.tsx
+++ src/pages/Landing/render.tsx
@@ -1,15 +1,10 @@
 import './style.scss';
 import * as React from 'react';
-
-import Thanks from 'Components/Thanks/render';
-import BasePage from 'Components/BasePage/render';
 import { RouteComponentProps } from 'react-router-dom';
 
 
-const LandingPage = (props:RouteComponentProps):JSX.Element => (
-    <BasePage {...props}>
-        <Thanks/>
-    </BasePage>
-);
+const LandingPage = (props:RouteComponentProps):JSX.Element => (<>
+
+</>);
 
 export default LandingPage;
