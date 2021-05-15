9f31c64bede7bad1dd6ceb3897d089f7ee220bf6
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2021-05-16 01:33:38 +0300

    use placeholder image instead of text

--- src/components/Tournaments/Card/New/render.tsx
+++ src/components/Tournaments/Card/New/render.tsx
@@ -5,7 +5,7 @@ import { RouteComponentProps } from 'react-router-dom';
 import { PATHS } from 'Constants';
 import { Tournament } from 'Utils/types';
 import { dateWithSlashes } from 'Utils/utils';
-
+import Trophy from 'Static/icons/trophy.svg'
 
 interface IProps extends RouteComponentProps {
     tournament: Tournament
@@ -19,7 +19,10 @@ function TournamentCardNew(props: IProps): JSX.Element {
     >
         {t.avatar.url
             ? <img className='tournament-card-new__img' src={t.avatar.url} alt={t.avatar.filename}/>
-            : <div style={{ width:'100%', height:150, textAlign:'center', lineHeight:'150px', backgroundColor:'lightgray', color:'white'}}>Иконки скоро будут</div>
+            :
+            <div className='tournament-card-new__placeholder'>
+                <img className={'tournament-card-new__placeholder_image'} src={Trophy} alt={'Tournament card'}/>
+            </div>
         }
         <p className='tournament-card-new__date'>{dateWithSlashes(new Date(t.created*1000))}</p>
         <h4 className='tournament-card-new__title'>{t.name}</h4>
